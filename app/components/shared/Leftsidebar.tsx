7e8a89b27a23e8e8e57e693024f66f45fb91bc43
krishnadev7 <krishnadevv12@gmail.com>
2024-05-03 11:55:29 +0530

    added leftsidebar

--- app/components/shared/Leftsidebar.tsx
+++ app/components/shared/Leftsidebar.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+"use client";
+import { sidebarLinks } from "@/app/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
+
 
 function Leftsidebar() {
+  const useRoute = useRouter();
+  const pathname = usePathname();
+
   return (
-    <div>Leftsidebar</div>
-  )
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex flex-1 w-full flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton>
+            <div className="flex gap-4 p-3 cursor-pointer">
+            <Image
+              src="/assets/logout.svg"
+              alt="logout"
+              height={24}
+              width={24}
+            />
+            <p className="max-lg:hidden text-light-2">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
 }
 
-export default Leftsidebar
+export default Leftsidebar;
--- app/components/shared/Topbar.tsx
+++ app/components/shared/Topbar.tsx
@@ -1,16 +1,39 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Topbar() {
   return (
-    <nav className='topbar'>
-      <Link href="/" className='flex items-center gap-4'>
-        <Image src="/assets/logo.svg" alt='logo' width={28} height={28}/>
-        <p className='text-light-1 text-heading3-bold max-xs:hidden'>Zaptext</p>
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-light-1 text-heading3-bold max-xs:hidden">Zaptext</p>
       </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                height={24}
+                width={24}
+              />
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher 
+            appearance={{
+              elements:{
+                organizationSwitcherTrigger:"py-2 px-4"
+              }
+            }}
+        />
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Topbar
+export default Topbar;
