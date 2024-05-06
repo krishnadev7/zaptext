5acb313f3f05fcc7e068013901ea6f4e28b0556f
krishnadev7 <krishnadevv12@gmail.com>
2024-05-06 12:29:05 +0530

    added rightsidebar

--- app/components/shared/Topbar.tsx
+++ app/components/shared/Topbar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -26,6 +27,7 @@ function Topbar() {
         </div>
         <OrganizationSwitcher 
             appearance={{
+              baseTheme: dark,
               elements:{
                 organizationSwitcherTrigger:"py-2 px-4"
               }
