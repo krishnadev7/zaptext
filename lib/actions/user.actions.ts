4fbf1f3dc3e340cb82040196a68e426d5910d242
krishnadev7 <krishnadevv12@gmail.com>
2024-05-17 21:08:08 +0530

    created user model & updateUser function

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.models";
+import { connectToDb } from "../mongoose";
+
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  image: string,
+  bio: string,
+  path: string
+): Promise<void> {
+  connectToDb();
+
+  try {
+    await User.findOneAndUpdate(
+        { id: userId },
+        {
+          username: username.toLowerCase(),
+          name,
+          image,
+          bio,
+          onboarded: true,
+        },
+        { upsert: true }
+      );
+    
+      if(path == "profile/edit"){
+        revalidatePath(path);
+      }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user ${error.message}`)
+  }
+  
+}
---
+++ lib/models/user.models.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { boolean } from "zod";
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: { type: String },
+  bio: { type: String },
+  threads: [{ type: mongoose.Schema.Types.ObjectId, ref: "Thread" }],
+  onboarded: { type: boolean, default: false },
+  communities: [{ type: mongoose.Schema.Types.ObjectId, ref: "Community" }],
+});
+
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
