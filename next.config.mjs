9c121690d9fa50626663af38e1a75720fdde4865
krishnadev7 <krishnadevv12@gmail.com>
2024-05-29 21:30:13 +0530

    completed ThreadCard component

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
             { protocol: "https", hostname: "images.clerk.dev" },
             { protocol: "https", hostname: "uploadthing.com" },
             { protocol: "https", hostname: "placehold.co" },
+            { protocol: "https", hostname: "utfs.io" },
         ],
     },
     typescript: {
