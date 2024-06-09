5cbf67691636016ebf5a803f0660ab4f2fc2b828
krishnadev7 <krishnadevv12@gmail.com>
2024-06-09 11:43:33 +0530

    added tabs in the profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -2,6 +2,15 @@ import ProfileHeader from "@/app/components/shared/ProfileHeader";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { profileTabs } from "@/app/constants";
+import Image from "next/image";
+import ThreadsTab from "@/app/components/shared/ThreadsTab";
 
 const page = async ({ params }: { params: { id: string } }) => {
   const user = await currentUser();
@@ -23,6 +32,36 @@ const page = async ({ params }: { params: { id: string } }) => {
         imageUrl={userInfo.image}
         bio={userInfo.bio}
     />
+    <div className="mt-9">
+      <Tabs defaultValue="threads" className="w-full">
+        <TabsList className="tab">
+          {profileTabs.map((tab) => (
+            <TabsTrigger key={tab.label} value={tab.value} className="tab">
+              <Image
+                src={tab.icon}
+                alt={tab.label}
+                width={24}
+                height={24}
+                className="object-contain"
+              />
+              <p className="max-sm:hidden">{tab.label}</p>
+             {tab.label === 'Threads' && (
+              <p className="ml-1 text-light-2 rounded-sm bg-red-500 px-2 py-1 !text-tiny-medium">{userInfo?.threads?.length}</p>
+             )}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {profileTabs.map((tab) => (
+          <TabsContent key={`content-${tab.label}`} value={tab.value} className="w-full text-light-1">
+              <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType='User'
+              />
+          </TabsContent>
+        ))}
+      </Tabs>
+    </div>
   </section>;
 };
 export default page;
---
+++ app/components/shared/ThreadsTab.tsx
@@ -0,0 +1,13 @@
+interface Props  {
+    currentUserId: string,
+    accountId: string,
+    accountType: string
+}
+
+const ThreadsTab = ({currentUserId,accountId,accountType}: Props) => {
+    return (
+        <div>ThreadsTab</div>
+    )
+}
+
+export default ThreadsTab;
