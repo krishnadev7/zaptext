250624834b31053d17ab0912537bb2482a35a783
krishnadev7 <krishnadevv12@gmail.com>
2024-06-17 21:08:42 +0530

    activity page completed

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -132,3 +132,27 @@ export async function fetchUsers({
     throw new Error(`Failed to fetch users ${error.message}`);
   }
 }
+
+export async function fetchActivity(userId: string){
+  try {
+    connectToDb();
+
+    const userThreads = await Thread.find({author: userId});
+
+    const childThreadIds = userThreads.reduce((acc,userThread) => {
+      return acc.concat(userThread.children);
+    },[]);
+
+    const replies = await Thread.find({
+      _id: {$in: childThreadIds},
+      author: {$ne: userId}
+    }).populate({
+      path: 'author',
+      model: 'User',
+      select: 'name image _id'
+    })
+    return replies
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity ${error.message}`);
+  }
+}
